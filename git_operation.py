1fe4f9a0050cbfe5fc7e87266c1d275e182f8cb8
Ruizhe Zhong <zerzerzerz271828@sjtu.edu.cn>
2022-05-07 14:35:21 +0800

    normalizing

--- git_operation.py
+++ git_operation.py
@@ -5,7 +5,7 @@ parser.add_argument("-m", type=str, default="modify")
 args = parser.parse_args()
 
 # commit_string = "TPR, FPR, TNR, FNR"
-not_add = ['data', 'weights',"archive.zip"]
+not_add = ['data',"archive.zip",'result']
 for item in os.listdir():
     if item in not_add:
         continue
